769ffa195d4031f16896945655ca6a094f2c08d7
math13001 <mathieu.fremont13@gmail.com>
2023-10-26 16:49:49 +0200

    first commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,3 +21,80 @@ const displayPokemons = async () => {
 }
 
 displayPokemons();
+
+const container = document.createElement("div");
+container.classList.add("container")
+      document.body.appendChild(container)
+      document.querySelectorAll("img")
+const select = document.createElement('select');
+      select.setAttribute("id","select");
+      select.classList.add("select")
+      container.appendChild(select)
+    let option = document.createElement("option");
+          option.innerText = " ----Sélectionnez un Pokémon----"
+          option.value = 0
+          option.setAttribute("selected", true)
+          select.appendChild(option)
+            console.log("Sélectionnez un Pokémon")
+            dataFetch.forEach( (pokemon) => {
+          option = document.createElement("option");
+          option.innerText = pokemon.name
+console.log("pokemon :", pokemon);
+select.appendChild(option)
+    
+});
+let selectPokemonImg = document.createElement("img")
+    selectPokemonImg.classList.add("affichage-pokemon")
+    container.appendChild(selectPokemonImg)
+    select.addEventListener("change",() => {
+        if (select == 0) {
+            // option.innerText ="Pokémon non sélectionner"
+            
+        }
+        else {
+            console.log("selection :" , select.value)
+            let imgPokemon = dataFetch.find((unPokemon) => unPokemon.name == select.value)
+            selectPokemonImg.setAttribute("src", imgPokemon.image)
+            selectPokemonImg.style.display = "block"
+        }
+    } )
+           
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// select.addEventListener('click', ()=>{
+// //     const selectedPokemon =  ('Sélectionnez un Pokémon (entrez un nom de Pokémon' + dataFetch.length + ' )');
+    
+// //     if(selectedPokemon && selectedPokemon >= 1 && selectedPokemon <= dataFetch.length){
+// //     const chosenPokemon = dataFetch[selectedPokemon -1];
+// //     console.log("Vous avez choisi :", chosenPokemon);   
+// //     }
+// //     else{
+// //         console.log("Pokémon invalide. Veuillez entrer un nom de Pokémon" + dataFetch.length);
+// //     }
+// });
+
